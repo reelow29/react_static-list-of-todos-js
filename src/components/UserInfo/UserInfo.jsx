c104f5a522f387894d4fd5bda0bbd0e4a089d5d1
Roma <rasevychr@gmail.com>
2024-10-16 11:03:53 +0300

    sol3

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-
 export const UserInfo = ({ user }) => {
   return (
     <div className="UserInfo">
       <p className="UserInfo__name">{user.name}</p>
+      <a className="UserInfo__email" href={`mailto:${user.email}`}>
+        {user.email}
+      </a>
     </div>
   );
 };
