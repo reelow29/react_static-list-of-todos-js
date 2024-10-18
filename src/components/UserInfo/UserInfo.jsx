ea1709034f6a65e5bedefd5340acb8b0da24eb7a
Roma <rasevychr@gmail.com>
2024-10-18 14:49:50 +0300

    solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 export const UserInfo = ({ user }) => {
+  const { name, email } = user;
+
   return (
-    <div className="UserInfo">
-      <p className="UserInfo__name">{user.name}</p>
-      <a className="UserInfo__email" href={`mailto:${user.email}`}>
-        {user.email}
-      </a>
-    </div>
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
   );
 };
